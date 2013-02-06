80077509c0d6b3888d8af5fa4d24211f77a36af5
Olivier Scherrer <pode.fr@gmail.com>
2013-02-06 21:22:42 +0000

    adding iframe

--- specs/Iframe.spec.js
+++ specs/Iframe.spec.js
@@ -4,10 +4,6 @@
  * Copyright (c) 2013 Olivier Scherrer <pode.fr@gmail.com>
  */
 
-beforeEach(function () {
-	this.addMatchers({
-		toBeInstanceOf: function(expected) {
-			return this.actual instanceof expected;
-		}
-	});
+require(["Iframe"], function (Iframe) {
+
 });
--- src/Iframe.js
+++ src/Iframe.js
@@ -4,10 +4,12 @@
  * Copyright (c) 2013 Olivier Scherrer <pode.fr@gmail.com>
  */
 
-beforeEach(function () {
-	this.addMatchers({
-		toBeInstanceOf: function(expected) {
-			return this.actual instanceof expected;
-		}
-	});
+define(
+
+/**
+* @class
+* iframe for managing iframes, one at a time
+*/
+function Iframe() {
+
 });
