231d405a8b9c47f2a20de05b040096ae2e7e946e
Olivier Scherrer <pode.fr@gmail.com>
2013-02-12 23:06:52 +0000

    adding iframes

---
+++ specs/Iframes.spec.js
@@ -0,0 +1,14 @@
+/**
+ * Scalable http://flams.github.com/scalable
+ * The MIT License (MIT)
+ * Copyright (c) 2013 Olivier Scherrer <pode.fr@gmail.com>
+ */
+
+require(["Iframes"], function (Iframe) {
+
+	describe("Iframes", function () {
+
+
+	});
+
+});
---
+++ src/Iframes.js
@@ -0,0 +1,15 @@
+/**
+ * Scalable http://flams.github.com/scalable
+ * The MIT License (MIT)
+ * Copyright (c) 2013 Olivier Scherrer <pode.fr@gmail.com>
+ */
+
+define(
+
+/**
+* @class
+*/
+function Iframes() {
+
+
+});
